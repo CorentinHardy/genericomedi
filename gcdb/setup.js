057caf398233b40f95e8443eb664da9a3ea1e593
NatNgs <nathael.nogues@hotmail.fr>
2018-08-03 21:17:59 +0200

    DB - Init DB

--- gcdb/setup.js
+++ gcdb/setup.js
@@ -1,2 +1,21 @@
+const MongoClient = require('mongodb').MongoClient;
 
-const db = connect("localhost/genericomedy")
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'genericomedy';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
+  if(err) {
+
+      console.log("FQIIIIIIIIIIIIILLLLLLLLLLL");
+  } else {
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+  }
+  try{
+    client.close();
+  }catch(e){}
+});
